71a75719778968519fa5714f39bceb90b2f0a78d
DanayitA <113548279+DanayitA@users.noreply.github.com>
2023-01-13 08:24:29 -0500

    Create playlist.js

---
+++ Webtvilletunes/js/playlist.js
@@ -0,0 +1,28 @@
+function handleButtonClick() {
+        var TextInput = document.getelementById("songTextInput");
+        var songName = textInput.value;
+        if (songName == "") {
+            alert("Please enter a song");
+        } else {
+            alert("Adding" + songName);
+        var li = document.createElement("li");
+        li.innerHTML = songName;
+        var ul = document.getElementById("playlist");
+        ul.appendChild(li);
+        save(songName)
+    }
+    }
+    function set() {
+  var loc = window.location.href;
+  var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+  var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+                        loc + '?profile=css3';
+  document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+  document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+}
+function init() {
+    var button = document.getElementById("addButton");
+    button.onclick = handleButtonClick;
+    loadPlaylist();
+    }
+    widnow.onload = init;
